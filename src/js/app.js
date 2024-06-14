55c3c25c24560e6cc20f989db6e993777bed2fc1
Dobby_lol <45934300+Dobbylol22@users.noreply.github.com>
2024-06-14 00:10:26 +0000

    blogg

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger')
+const sidebar = document.querySelector('#sidebar')
+const links = document.querySelectorAll('#sidebar ul li a')
+burger.addEventListener('click',()=>{
+    sidebar.classList.toggle('is-active')
+    burger.classList.toggle('is-active') /*this will display/close the sidebar when we click on the burger*/
+})
+
+links.forEach(link => link.addEventListener('click',()=>{
+    links.forEach(link => link.classList.remove('is-active')) /*this is to remove the active (is-active) of home link and shift it to the whatever the link we click on*/
+    link.classList.add('is-active')
+}))
